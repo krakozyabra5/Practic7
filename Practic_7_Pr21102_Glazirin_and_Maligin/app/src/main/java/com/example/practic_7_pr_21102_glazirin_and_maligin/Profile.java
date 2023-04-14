333d4ab552f44d12efcbbdf55db339a2fed71dc6
Krakozyabra5 <91729303+krakozyabra5@users.noreply.github.com>
2023-04-14 12:02:02 +0700

    пупа

---
+++ Practic_7_Pr21102_Glazirin_and_Maligin/app/src/main/java/com/example/practic_7_pr_21102_glazirin_and_maligin/Profile.java
@@ -0,0 +1,14 @@
+package com.example.practic_7_pr_21102_glazirin_and_maligin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
